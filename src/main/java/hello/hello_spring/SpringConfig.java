0476e74fc253c9c311601abc2e7a719b7601e0f0
minseo08 <superider00@gmail.com>
2024-08-03 18:15:12 +0900

    jpa basic

---
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -0,0 +1,43 @@
+package hello.hello_spring;
+import hello.hello_spring.aop.TimeTraceAop;
+import hello.hello_spring.repository.*;
+import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import javax.sql.DataSource;
+@Configuration
+public class SpringConfig {
+    //private final EntityManager em;
+    private final MemberRepository memberRepository;
+    //@Autowired
+    //public SpringConfig(EntityManager em){
+        //this.em = em;
+    //}
+    public SpringConfig(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+    //@Bean
+    //public MemberService memberService(){
+        //return new MemberService(memberRepository());
+    //}
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
+    }
+
+    @Bean
+    public TimeTraceAop timeTraceAop(){
+        return new TimeTraceAop();
+    }
+    //@Bean
+    //public MemberRepository memberRepository(){
+        //return new MemoryMemberRepository();
+        //return new JdbcMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        //return new JpaMemberRepository(em);
+    //}
+}
---
+++ src/main/java/hello/hello_spring/aop/TimeTraceAop.java
@@ -0,0 +1,18 @@
+package hello.hello_spring.aop;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+@Aspect
+public class TimeTraceAop {
+    @Around("execution(* hello.hellospring..*(..))")
+    public Object execut(ProceedingJoinPoint joinPoint) throws Throwable{
+        long start = System.currentTimeMillis();
+        try{
+            return joinPoint.proceed();
+        } finally{
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}
