2f0731822f2e89f2fe0c4ff6078cee93b5eb78b6
minseo08 <superider00@gmail.com>
2024-08-03 18:30:28 +0900

    interface modify

--- src/main/java/hello/hello_spring/repository/MemberRepository.java
+++ src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -5,6 +5,8 @@ import hello.hello_spring.domain.Member;
 import java.util.List;
 import java.util.Optional;
 
+
+
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
